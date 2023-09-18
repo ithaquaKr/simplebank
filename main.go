d396413d1db5d609798fd9fee3bf46a69bb8f4f6
Tuan Hiep <hiepnguyentuan1002@gmail.com>
2023-09-18 11:01:28 +0700

    feat: add viper to load config from env

--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 
 	"github.com/ithaquaKr/simplebank/api"
 	db "github.com/ithaquaKr/simplebank/db/sqlc"
+	"github.com/ithaquaKr/simplebank/utils"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://backend:password@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Can not load config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
@@ -24,6 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
+	var serverAddress = config.AppHost + ":" + config.AppPort
 	err = server.Start(serverAddress)
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
